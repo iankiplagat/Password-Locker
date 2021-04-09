95f0ba282abfbc374f05119048d43ede92054c55
Kasparov <ianjkiplagat@gmail.com>
2021-04-09 06:51:27 +0300

    add method that defines user properties

--- password.py
+++ password.py
@@ -1,6 +1,20 @@
 class User:
-        """
-        Class that generates new instances of users
-        """
+    """
+    Class that generates new instances of users
+    """
 
-        pass
+    pass
+
+    def __init__(self,first_name,last_name,email, user_name, password):
+
+        '''
+        __init__ method that helps us define properties for our objects.
+
+        Args:
+            first_name: New contact first name.
+            last_name : New contact last name.
+            email : New contact email address.
+            user_name: New user account user name.
+            password: New user account credentials.
+        '''
+        
